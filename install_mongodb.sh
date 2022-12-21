344ed041d0d4fa3684bed20acb8358074a9bd5d7
nwomib <nwomib@yandex.ru>
2022-12-21 22:02:36 +0600

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -6,6 +6,6 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongod
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl daemon-reload
-sudo systemctl start mongodb
-sudo systemctl enable mongodb
-sudo systemctl status mongodb
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
