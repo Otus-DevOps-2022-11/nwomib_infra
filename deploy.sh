cd115a9a8ce0ce67b08783a1c401feec0374e41d
nwomib <nwomib@yandex.ru>
2022-12-21 21:46:30 +0600

    no +e

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash +e
+#!/usr/bin/env bash 
 sudo apt-get update
 sudo apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,15 +1,11 @@
-#!/usr/bin/env bash +e
+#!/usr/bin/env bash
 sudo apt update
 sudo apt-get install apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sleep 5
 sudo systemctl daemon-reload
-sleep 5
 sudo systemctl start mongod
-sleep 5
 sudo systemctl enable mongod
-sleep 5
 sudo systemctl status mongod
