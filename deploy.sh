03c11ad7097cb9c5d0faaa5cd428c43cd0d515f7
nwomib <nwomib@yandex.ru>
2022-12-21 20:50:25 +0600

    fixed ex and name of vpn

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install -y git mc
 git clone -b monolith https://github.com/express42/reddit.git
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,2 +1,2 @@
-#!/bin/bash -x
+#!/usr/bin/env bash
 sudo apt install -y ruby-full ruby-bundler build-essential
