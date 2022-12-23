7b524c6ef564a8377ebf4558fd6a8866e7a84f24
nwomib <nwomib@yandex.ru>
2022-12-23 16:49:14 +0600

    HW-05

---
+++ packer/scripts/install_reddit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install -y git
+git clone -b monolith https://github.com/express42/reddit.git
+
+touch /etc/systemd/system/puma.service
+echo -e "[Unit]\nDescription=puma\n[Service]\nType=oneshot ExecStart=cd /home/ubuntu/reddit && bundle install && puma -d\nExecStop=\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/puma.service
+
+
+sudo update-rc.d имясервиса enable	
