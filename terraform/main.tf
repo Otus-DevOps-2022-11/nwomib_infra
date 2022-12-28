b16027862da2bb962a09b50922b9f6802853982d
nwomib <nwomib@yandex.ru>
2022-12-29 01:46:59 +0600

    final

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,16 @@
 provider "yandex" {
   token     = "y0_AgAAAAAAgQVtAATuwQAAAADXXBr0vNf3KidmTqWh9t_u9sz6ZRsocnA"
-  cloud_id  = "b1g5togtnendvqppvoie"
-  folder_id = "b1gv355u3l2dspkgvov0"
-  zone      = "ru-central1-a"
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
+  zone = var.zone
   metadata = {
-  ssh-keys = "ubuntu:${file("appuser.pub")}"
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
-  
+
   resources {
     cores  = 2
     memory = 2
@@ -18,27 +19,27 @@ resource "yandex_compute_instance" "app" {
   boot_disk {
     initialize_params {
       # Указать id образа созданного в предыдущем домашем задании
-      image_id = "fd84gcfsk0v9p1ac4oh8"
+      image_id = var.image_id
     }
   }
 
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9b9hmsht01le47daoe3"
+    subnet_id = var.subnet_id
     nat       = true
   }
-  
+
   connection {
-	type = "ssh"
-	host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-	user = "ubuntu"
-	agent = false
-	# путь до приватного ключа
-    private_key = "${file("priv")}"
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file("${var.private_key}")
   }
-  
+
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
 
