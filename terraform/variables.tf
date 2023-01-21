cf4c3ffb6ed8517a805846b502b69c7485c8fd1f
nwomib <nwomib@yandex.ru>
2023-01-21 20:32:25 +0600

    terra2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,3 +25,11 @@ variable service_account_key_file {
 variable private_key {
   description = "priv"
 }
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
+}
